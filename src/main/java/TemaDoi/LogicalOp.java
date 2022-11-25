de2fd1dce31bb5d34acf30a94030146cb06ed308
MarilenaCretu <marilenac2001@gmail.com>
2022-11-25 18:55:18 +0200

    commit

--- src/main/java/TemaDoi/LogicalOp.java
+++ src/main/java/TemaDoi/LogicalOp.java
@@ -24,36 +24,41 @@ public class LogicalOp {
             System.out.println(list.get(i));
         }
     }
+
     //Exercitiul 4
     public void printListInvers(List<Integer> list) {
         for (int i = list.size() - 1; i >= 0; i--) {
             System.out.print(list.get(i) + " ");
         }
     }
+
     //Exercitiul 5
     public void addString(List<String> listS, int position, String text) {
         listS.add(position, text);
         System.out.println(listS);
     }
+
     //Exercitiul 6
     public void addOList(List<Integer> list, int number) {
         list.add(0, number);
         System.out.println(list);
     }
+
     //Exercitiul 7
     public void printListValue(List<Integer> list) {
         for (int i = 0; i < list.size(); i++)
             System.out.println("Index " + i + " are elementul " + list.get(i));
     }
-        //Exercitiul 8
-        public int getMaxFromList(List<Integer> list) {
-            int max = list.get(0);
-            for (int i = 0; i < list.size(); i++) {
-                if (list.get(i) > max) {
-                    max = list.get(i);
-                }
+
+    //Exercitiul 8
+    public int getMaxFromList(List<Integer> list) {
+        int max = list.get(0);
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) > max) {
+                max = list.get(i);
             }
-            System.out.println(max);
-            return max;
         }
+        System.out.println(max);
+        return max;
+    }
 }
